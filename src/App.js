3abca23d88bebfda448dfec5fd99ae92dfa342e3
Victor Ruiz <vik_ases@live.com.m.x>
2019-11-07 12:27:50 -0600

    Refactorizando y modificando el primer componente

--- src/App.js
+++ src/App.js
@@ -11,16 +11,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <MiComponente/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <section className='componentes'>
+        <MiComponente/>
+        <MiComponente/>
+        <MiComponente/>
+
+      </section>
     </div>
   );
 }
--- src/components/MiComponente.js
+++ src/components/MiComponente.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-class MiComponente extends React.Component {
+//Refactorizando la clase
+class MiComponente extends Component {
     render(){
+
+        let receta = {
+            nombre:'Pizza',
+            ingredientes:['Tomate', 'queso']
+        }
         return (
-            <div>
-                <h1>Hola Mundo</h1>
+            
+            //Se debe de regresar siempre solo un nodo
+            //Se puede usar React.Fragment
+            //Lo recomendable es ponerlo en un div
+            <div className='mi-componente'>
+                <h1>{receta.nombre}</h1>
+                <ul>
+                    {
+                        receta.ingredientes.map((ingrediente, i ) => {
+                            console.log(ingrediente)
+                            return (
+                                <li key={i}>
+                                    {ingrediente}
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+                <hr/>
             </div>
+
         )
     }
 }
